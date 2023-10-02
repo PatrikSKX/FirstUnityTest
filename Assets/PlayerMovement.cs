5eda38327a552f5cf33de9d9d0fd788ee1df6adb
PatrikSK <patrik.nemsovsky@gmail.com>
2023-10-02 15:29:05 +0200

    Dash

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,24 +6,59 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float movespeed = 5f;
+    public float dashspeed = 15f;
+    private float dashDuration = 0.15f;
+    private bool isDash = false;
 
     public Rigidbody2D player;
     public Animator anim;
+    public Collider2D playerCol;
     Vector2 movement;
+    public float ActMovespeed;
 
     // Update is called once per frame
+    private void Start()
+    {
+        ActMovespeed = movespeed;
+    }
+
     void Update()
     {
+        if (isDash)
+        {
+            return;
+        }
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.sqrMagnitude);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            StartCoroutine(Dash());
+        }
+
     }
 
     private void FixedUpdate()
     {
-        player.MovePosition(player.position + movement * movespeed * Time.fixedDeltaTime);
+        player.MovePosition(player.position + movement.normalized * ActMovespeed * Time.fixedDeltaTime);
+    }
+
+    private IEnumerator Dash()
+    {
+        isDash = true;
+        playerCol.enabled = false;
+        anim.SetFloat("Dash", 1f);
+        ActMovespeed = dashspeed;
+        //player.MovePosition(player.position + movement * dashspeed * Time.fixedDeltaTime);
+        yield return new WaitForSeconds(dashDuration);
+        anim.SetFloat("Dash", 0f);
+        ActMovespeed = movespeed;
+        playerCol.enabled = true;
+        isDash = false;
     }
 }
